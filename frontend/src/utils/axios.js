2b58ddadbfb5d2b5a6facb11b60932b813cda3f9
Ramziyahozi <ramziyahozi@gmail.com>
2025-07-21 09:55:19 +0700

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 // Buat instance axios dengan konfigurasi default
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL + '/api',
   headers: {
     'Content-Type': 'application/json',
   }
-});
+});;
 
 // Interceptor untuk request
 instance.interceptors.request.use(
